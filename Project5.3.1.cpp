3663db87680d5e06481723e0810fe9e7457aa52c
sonka2598 <69012990+sonka2598@users.noreply.github.com>
2026-08-25 12:20:40 +0600

    Add files via upload

---
+++ Project5.3.1.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+using namespace std;
+
+void merge(int* arr, int left, int mid, int right) {
+	int n1 = mid - left + 1; 
+	int n2 = right - mid;
+	
+	int* LeftArr = new int[n1];
+	int* RightArr = new int[n2];
+	
+	for (int i = 0; i < n1; i++) 
+		LeftArr[i] = arr[left + i];
+
+	for (int j = 0; j < n2; j++) 
+		RightArr[j] = arr[mid + 1 + j];
+	
+	int i = 0, j = 0, k = left;
+
+	while (i < n1 && j < n2) {
+		if (LeftArr[i] <= RightArr[j]) {
+			arr[k] = LeftArr[i];
+			i++;
+		}
+		else {
+			arr[k] = RightArr[j];
+			j++;
+		}
+		k++;
+	}
+
+	while (i < n1) {
+		arr[k] = LeftArr[i];
+		i++;
+		k++;
+	}
+
+	while (j < n2) {
+		arr[k] = RightArr[j];
+		j++;
+		k++;
+	}
+
+	delete[] LeftArr;
+	delete[] RightArr;
+}
+
+void merge_sort(int* arr, int left, int right) {
+	if (left < right) {
+		int mid = left + (right - left) / 2;
+		merge_sort(arr, left, mid); 
+		merge_sort(arr, mid + 1, right); 
+		merge(arr, left, mid, right);
+	}
+}
+
+void merge_sort(int* arr, int size) {
+	merge_sort(arr, 0, size - 1);
+}
+
+void printArray(int* arr, int size) {
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int arr1[] = {3, 43, 38, 29, 18, 72, 57, 61, 2, 33};
+	int arr2[] = {88, 91, 87, 59, 53, 49, 29, 16, 4, 27, 28, 89, 2, 25, 74 };
+	int arr3[] = {24, 66, 20, 79, 30, 16, 19, 62, 94, 59, 0, 7, 59, 90, 84, 60, 95, 62 };
+
+	int size1 = sizeof(arr1) / sizeof(arr1[0]);
+	int size2 = sizeof(arr2) / sizeof(arr2[0]);
+	int size3 = sizeof(arr3) / sizeof(arr3[0]);
+
+	cout << "Исходный массив: ";
+	printArray(arr1, size1);
+	merge_sort(arr1, size1);
+	cout << "Отсортированный массив: ";
+	printArray(arr1, size1);
+	cout << endl;
+
+	cout << "Исходный массив: ";
+	printArray(arr2, size2);
+	merge_sort(arr2, size2);
+	cout << "Отсортированный массив: ";
+	printArray(arr2, size2);
+	cout << endl;
+
+	cout << "Исходный массив: ";
+	printArray(arr3, size3);
+	merge_sort(arr3, size3);
+	cout << "Отсортированный массив: ";
+	printArray(arr3, size3);
+	return 0;
+}
